bf219fb7b7daee38ca369e1a2ca544d781a9fe99
Roger Monteiro <roger-15monteiro@hotmail.com>
2020-06-16 19:30:43 -0400

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import Header from './components/Header'
 
 function App(){
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject(){
+        setProjects([ ... projects, `Project ${Date.now()}`])
+        console.log(projects)
+    }
     return (
-        <>
-            <Header />
-            <Header />
+        <>            
+            <Header title="Projects"/>
+                <ul>
+                    {projects.map(project => <li key={project}>{project}</li>)}
+                </ul>
+                <button type="button" onClick={handleAddProject}>Adcionar projeto</button>
         </>
     )
 }
