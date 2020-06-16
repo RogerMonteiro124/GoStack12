35b728719e4c390cadacf7c39430110586da4bac
Roger Monteiro <roger-15monteiro@hotmail.com>
2020-06-15 22:02:45 -0400

    Initial Commit

---
+++ backend/src/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const {uuid} = require('uuidv4')
+const app = express();
+app.use(express.json());
+
+
+const projects = [];
+
+
+app.get('/projects', (req,res)=>{
+    const {title} = req.query;
+    console.log(title);
+
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects;
+
+    return res.json(results)
+})
+
+app.post('/projects',(req, res)=>{
+    const {title, owner} = req.body;
+    
+    const project = {id:uuid(), title, owner}
+
+    projects.push(project)
+
+    return res.json(project)
+})
+
+app.put('/projects/:id',(req, res)=>{
+    const {id} = req.params;
+    
+    const {title, owner} = req.body;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return res.status(400).json({erro:"Project not found"})
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project;
+
+    return res.json(project)
+})
+app.delete('/projects/:id',(req, res)=>{
+    const {id} = req.params;
+    
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if(projectIndex < 0){
+        return res.status(400).json({erro:"Project not found"})
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return res.status(204).send()
+})
+
+
+app.listen(3000,()=>{
+    console.log('Back-end Start! 🚀')
+});
